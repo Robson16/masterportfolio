9cdef2b8889f442dfbf9a43abc141b3d030ce72e
Robson <robhenrod@gmail.com>
2019-11-28 11:47:28 -0300

    Yoast SEO Update

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1bd55d777b2d24659deb063f5849e90d::getLoader();
+return ComposerAutoloaderInitabfc8685fac717f98c336d72034f94db::getLoader();
