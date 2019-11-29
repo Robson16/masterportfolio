44b53e0f3a2fc369a0b61756b758ede67e8f148c
Robson <robhenrod@gmail.com>
2019-11-29 16:07:01 -0300

    Organização do template-parts
    - Organização das template-parts para melhor compreensão

--- wp-content/themes/masterportfolio/template-frontpage-1.php
+++ wp-content/themes/masterportfolio/template-frontpage-1.php
@@ -22,12 +22,57 @@
             <?php the_content(); ?>
         </div>
         <!-- /.container -->
+
+        <div class="bg-light">
+            <div class="container py-2 py-lg-5">
+                <div class="row flex-column flex-sm-row">
+
+                    <?php if (is_active_sidebar('frontpage-widget-one')) : ?> 
+                        <div class="col my-3">
+                            <div class="h-100 bg-white p-3">
+                                <?php dynamic_sidebar('frontpage-widget-one'); ?>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                    <?php endif; ?>
+
+                    <?php if (is_active_sidebar('frontpage-widget-two')) : ?> 
+                        <div class="col my-3">
+                            <div class="h-100 bg-white p-3">
+                                <?php dynamic_sidebar('frontpage-widget-two'); ?>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                    <?php endif; ?>
+
+                    <?php if (is_active_sidebar('frontpage-widget-three')) : ?> 
+                        <div class="col my-3">
+                            <div class="h-100 bg-white p-3">
+                                <?php dynamic_sidebar('frontpage-widget-three'); ?>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                    <?php endif; ?>
+
+                    <?php if (is_active_sidebar('frontpage-widget-four')) : ?> 
+                        <div class="col my-3">
+                            <div class="h-100 bg-white p-3">
+                                <?php dynamic_sidebar('frontpage-widget-four'); ?>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                    <?php endif; ?>
+
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+        </div>
     </section>
 
-    <?php get_template_part('template-parts/frontpage/frontpage', 'widget'); ?>
-    <?php get_template_part('template-parts/frontpage/frontpage', 'skills'); ?>
-    <?php get_template_part('template-parts/frontpage/frontpage', 'resume'); ?>
-    <?php get_template_part('template-parts/frontpage/frontpage', 'portfolio'); ?>
+    <?php get_template_part('template-parts/sections/section', 'skills'); ?>
+    <?php get_template_part('template-parts/sections/section', 'resume'); ?>
+    <?php get_template_part('template-parts/sections/section', 'portfolio'); ?>
     <?php get_template_part('template-parts/sections/section', 'contact'); ?>
 </main>
 
