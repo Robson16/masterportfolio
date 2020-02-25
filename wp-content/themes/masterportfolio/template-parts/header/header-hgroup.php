5d945bc2c9dc5f697368eb536f6924836bc3f903
Robson Henrique Rodrigues <robhenrod@gmail.com>
2020-02-25 17:49:58 -0300

    Cabeçalhos
    - Códigos para os títulos otimizados;

--- wp-content/themes/masterportfolio/template-parts/header/header-hgroup.php
+++ wp-content/themes/masterportfolio/template-parts/header/header-hgroup.php
@@ -1,36 +1,31 @@
 <hgroup>
-    <?php if (is_single()): ?>
+    <?php
 
-        <h1 class="title title-cursive"><?php echo get_the_title(); ?></h1>
+    if ( is_search() ) {
+        echo sprintf( '<h1 class="d-inline-block title title-cursive">%s <span>%s</span></h1>', __( 'Search results for:', 'masterportfolio'), get_search_query() );
+    }
 
-    <?php elseif (is_search()): ?>
+    if ( is_archive() ) {
+        echo sprintf( '<h1 class="title title-cursive">%s</h1>', get_the_archive_title() );
+    }
 
-        <h1 class="d-inline-block title title-cursive"><?php _e('Search results for:', 'masterportfolio'); ?></h1>
-        <span class="h3 title"><?php echo get_search_query(); ?></span>
+    if ( is_home() ) {
+        echo sprintf( '<h1 class="title title-cursive">%s</h1>', __( 'Blog', 'masterportfolio' ) );
+    }
 
-    <?php elseif (is_post_type_archive('portfolio')): ?>
+    if ( is_404() ) {
+        echo sprintf( '<h1 class="title title-cursive">%s</h1>', __( 'Not Found', 'masterportfolio' ) );
+    }
 
-        <h1 class="title title-cursive"><?php echo post_type_archive_title('', false); ?></h1>
+    if ( is_single() || is_page() ) {
+        echo sprintf( '<h1 class="title title-cursive">%s</h1>', get_the_title() );
+    }
 
-    <?php elseif (is_archive()): ?>
+    if ( rwmb_meta('subtitle') ) {
+        echo sprintf( '<h3>%s</h3>', rwmb_meta( 'subtitle' ) );
+    }
 
-        <h1 class="title title-cursive"><?php the_archive_title(); ?></h1>
-
-    <?php elseif (is_blog()): ?>
-
-        <h1 class="title title-cursive"><?php _e('Blog', 'masterportfolio'); ?></h1>
-
-    <?php else: ?>  
-
-        <h1 class="title title-cursive"><?php echo get_the_title(); ?></h1>
-
-    <?php endif; ?>
-
-    <?php if (rwmb_meta('subtitle')): ?>
-
-        <h2><?php echo rwmb_meta('subtitle'); ?></h2>
-
-    <?php endif; ?>
+    ?>
 </hgroup>
 
 <hr class="mt-0">
