32a75c5b2bbed6f061e601b5cfa350db391d61b0
Robson Henrique Rodrigues <robhenrod@gmail.com>
2020-02-25 17:42:12 -0300

    Maps Condition
    - Condição para exibir mapas apenas se tiver elemento maps no template exibido;

--- wp-content/themes/masterportfolio/js/initmap.js
+++ wp-content/themes/masterportfolio/js/initmap.js
@@ -5,23 +5,25 @@ $(function () {
 function initMap() {
     // Element container for the map
     var mapContainer = document.getElementById('map');
-    
-    // Get latitude and longitude data from container
-    var coordinates = {
-        lat: parseFloat(mapContainer.dataset.lat),
-        lng: parseFloat(mapContainer.dataset.lng)
-    };
-    
-    // Init a new google map object
-    var map = new google.maps.Map(mapContainer, {
-        center: coordinates,
-        zoom: 15,
-        disableDefaultUI: true
-    });
-    
-    // Init a new marker for the map with the coodinates
-    var marker = new google.maps.Marker({
-        position: coordinates,
-        map: map
-    });
+
+    if (mapContainer) {
+        // Get latitude and longitude data from container
+        var coordinates = {
+            lat: parseFloat(mapContainer.dataset.lat),
+            lng: parseFloat(mapContainer.dataset.lng)
+        };
+
+        // Init a new google map object
+        var map = new google.maps.Map(mapContainer, {
+            center: coordinates,
+            zoom: 15,
+            disableDefaultUI: true
+        });
+
+        // Init a new marker for the map with the coodinates
+        var marker = new google.maps.Marker({
+            position: coordinates,
+            map: map
+        });
+    }
 }
