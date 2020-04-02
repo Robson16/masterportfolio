73d18fad4b38e21e3fc6c5726143682c41622a77
Robson Henrique Rodrigues <robhenrod@gmail.com>
2020-04-02 17:24:57 -0300

    Apenas o tema

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file
  *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
- * @var bool
  */
-define( 'WP_USE_THEMES', true );
+?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<?php get_header(); ?>
+
+<main role="main">
+    <div class="container pb-5 pt-3">
+        <div class="row">
+            <div class="col-12 col-md-8">
+
+                <div class="row">
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <div class="col-12">
+                                <?php get_template_part('template-parts/content/content', 'excerpt'); ?>
+                            </div>
+                            <!-- /.col -->
+                        <?php endwhile; ?>
+                        <div class="col-12">
+                            <?php bootstrap_pagination(); ?>
+                        </div>
+                        <!--/.col-->
+                    <?php else: ?>
+                        <div class="col-12">
+                            <?php get_template_part('template-parts/content/content', 'none'); ?>
+                        </div>
+                        <!--/.col-->
+                    <?php endif; ?>
+                </div>
+                <!--/.row-->
+
+            </div>
+            <!--/.col-->
+            <div class="col-12 col-md-4">
+                <?php get_sidebar(); ?>
+            </div>
+            <!--/.col-->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</main>
+
+
+<?php get_footer(); ?>
